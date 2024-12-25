fde22d0686fd8fe1ee8fc1cb6ef9af277c7eb98d
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-12-25 17:32:17 +0700

    feat: create home layout

--- resources/js/Layouts/HomeLayout/Header.jsx
+++ resources/js/Layouts/HomeLayout/Header.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Button } from '@/Components/ui/button';
 import { Sheet, SheetTrigger, SheetContent } from '@/Components/ui/sheet';
 import {
@@ -11,7 +12,7 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { ChevronDown, MenuIcon, Mountain } from 'lucide-react';
 
-export default function Component() {
+const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-white dark:border-gray-800 dark:bg-gray-950">
       <div className="container mx-auto flex h-16 max-w-6xl items-center justify-between px-4 md:px-6">
@@ -104,4 +105,6 @@ export default function Component() {
       </div>
     </header>
   );
-}
+};
+
+export default Header;
