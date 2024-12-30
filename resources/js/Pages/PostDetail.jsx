8221c1afd23e33b840ce39550c07f220ca764646
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-12-31 04:28:10 +0700

    feat: fetch comment in post detail user

--- resources/js/Pages/PostDetail.jsx
+++ resources/js/Pages/PostDetail.jsx
@@ -1,12 +1,12 @@
 import PostCard from '@/Components/PostCard';
 import HomeLayout from '@/Layouts/HomeLayout';
 
-export default function PostDetail({ post }) {
+export default function PostDetail({ post, comments }) {
   return (
     <HomeLayout>
       <div className="flex flex-col justify-center items-center">
         <div className="max-w-3xl w-full flex flex-col gap-4">
-          <PostCard isDetail post={post} />
+          <PostCard isDetail post={post} comments={comments} />
         </div>
       </div>
     </HomeLayout>
