09a370aaf4d36b674d3387e20a36acffe6826794
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2025-01-02 00:34:30 +0700

    fix: disabled if no comment

--- resources/js/Components/CommentAdd.jsx
+++ resources/js/Components/CommentAdd.jsx
@@ -46,7 +46,7 @@ const CommentAdd = ({ postId }) => {
         <InputError message={errors.content} className="mt-2" />
       </div>
       <div className="flex justify-end mt-4">
-        <Button type="submit" disabled={processing}>
+        <Button type="submit" disabled={!data.content || processing}>
           Comment
         </Button>
       </div>
